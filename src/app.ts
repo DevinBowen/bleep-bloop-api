5919fe06149eae5512085ef2c1b74f25aecbb725
DevinBowen <bowen61496@gmail.com>
2025-08-05 12:44:57 -0400

    production build

--- src/app.ts
+++ src/app.ts
@@ -9,17 +9,23 @@ import Sql from './routers/sql'
 export default async function App(): Promise<FastifyInstance> {
     const app = fastify({
         logger: false,
+        trustProxy: true
     })
 
     await app.register(cors, {
-        origin: ['http://localhost:8080'],
+        origin: [
+            'http://localhost:8080',
+            'https://gatorridgefarm.com'
+        ],
         credentials: false,
     })
 
-    app.register(Auth, { prefix: '/auth' })
-    app.register(Files, { prefix: '/files' })
-    app.register(Email, { prefix: '/email' })
-    app.register(Sql, { prefix: '/sql' })
+    await app.register(async function (api) {
+        api.register(Auth, { prefix: '/auth' })
+        api.register(Files, { prefix: '/files' })
+        api.register(Email, { prefix: '/email' })
+        api.register(Sql, { prefix: '/sql' })
+    }, { prefix: '/api' })
 
     return app
 }
